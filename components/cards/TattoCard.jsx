ba8764e51b32f6744233a60bdc227e320e2826e1
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2025-09-12 12:24:34 +0600

    sdf

--- components/cards/TattoCard.jsx
+++ components/cards/TattoCard.jsx
@@ -7,7 +7,7 @@ const TattoCard = ({ tatto }) => {
       <img
         src={tatto.imageUrl}
         alt={tatto.title}
-        className="absolute inset-0 w-full h-full object-cover blur-2xl scale-110"
+        className="absolute inset-0 w-full h-full object-cover blur-xl scale-110"
       />
 
       {/* Main image */}
